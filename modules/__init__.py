220098865925a8f65b7c92e8871295908dc0e34b
Tristan Zine <72631199+TJZine@users.noreply.github.com>
2025-09-30 03:26:08 -0400

    Document compare.py controls

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,2 +1,9 @@
-from .utils import *
+"""Helper exports for the :mod:`modules` package."""
+
+from .utils import *  # noqa: F401,F403
 from .vs_preview.view import Preview
+
+__all__ = [
+    name for name in globals().keys()
+    if not name.startswith('_')
+]
